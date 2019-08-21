dfafff47b27905bcba6e707bf13ac864dfa2bd24
Jared Brodsky <jarbro@users.noreply.github.com>
2019-08-21 09:59:25 -0400

    Create intune-no-autopilot-provision.ps1

---
+++ intune-no-autopilot-provision.ps1
@@ -0,0 +1,24 @@
+## Renames computer to corrent naming convention
+## Removes initial user from Admin group
+## Prompts user and forces restart
+
+start-sleep -seconds 150
+
+$checkfile = Test-Path $env:USERPROFILE\\provisioned.txt
+if($checkfile) {exit} else {
+
+$user = whoami
+#Remove-LocalGroupMember -Group "Administrators" -Member $user ##not working in v1903 8/20/2019
+
+net localgroup "Administrators" $user /delete
+
+$serial = Get-Ciminstance -classname win32_bios | select -expandproperty serialnumber
+$computername = "MG-" + $serial
+rename-computer -newname $computername -force
+
+
+shutdown /r /t 15 /c "We need to restart your computer to finish setting up your account."
+
+sc $env:USERPROFILE\provisioned.txt 'This file was created as part of the provisioning process.'
+
+}
